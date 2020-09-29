dfe8d1bebd4ea065294afcb830641e6e51b38f73
JohanRune <kjgrune@yahoo.se>
2020-09-29 14:39:01 +0200

    Nu utökad igen

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ett gittest");
         System.out.println("Nu utökad.");
+
+        System.out.println("Nu ännu en gång utökad.");
     }
 }
